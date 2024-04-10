b78c3c1c5e45a24e0048b708f3cccdb47f3f3b21
Asher Etherington <asherce21@gmail.com>
2024-04-10 15:47:28 -0700

    users class created and method to add user to database is working

---
+++ database/db.js
@@ -0,0 +1,6 @@
+// uri for mongodb database
+module.exports = {
+    mongoURI: 'mongodb+srv://asherce21:A252478ssd@cluster0.1xxrv63.mongodb.net/',
+    dbName: "slugmanager",
+    usersCollection: "users"
+};
---
+++ database/usersClass.js
@@ -0,0 +1,63 @@
+const { MongoClient } = require('mongodb');
+const config = require("./db.js");
+
+/*
+SCHEMA FOR USERS BELOW
+
+user = {
+    _id: auto generated,
+    username: should be given,
+    password: should be the hash of the given password,
+    projects: an array of projects related to the user. Initially empty.
+}
+*/
+
+
+// A class for users with all needed methods to access and alter the users data collection
+class Users {
+    //constructor sets uri database name and the collection to add to
+    constructor() {
+        this.uri = config.mongoURI;
+        this.dbName = config.dbName;
+        this.collectionName = config.usersCollection;
+    }
+
+    // method used to create a new user given a username and password
+    async createUser(username, password) {
+    const client = new MongoClient(this.uri);
+    
+    try {
+        await client.connect(); // Connect to MongoDB
+        //get the database
+        const database = client.db(this.dbName);
+        //get the collection
+        const collection = database.collection(this.collectionName);
+        //initialize a new user
+        const newUser = {
+        username: username,
+        password: password,
+        projects: []
+        };
+        //insert the new user into the collection
+        const result = await collection.insertOne(newUser);
+        //print a success message
+        console.log(`User ${username} created successfully.`);
+        return result.insertedId; // Return the ID of the inserted document
+        } catch (error) {//if there was an error print the error
+        console.error('Error creating user:', error);
+        }finally {
+            await client.close(); // Close the connection
+        }
+    }
+}
+
+// Usage example for users class and some associated methods
+const uri = config.mongoURI;
+
+//iinitialize a new Users class
+const users = new Users(uri);
+
+//create some new users that should be added to the mongodb database
+users.createUser("asher", "1234");
+users.createUser("cali", "1234");
+users.createUser("kiera", "1234");
