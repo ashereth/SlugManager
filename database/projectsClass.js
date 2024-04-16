e25a2bc1a100b70cc2f6953c91776d70f46ed668
Asher <asherce21@gmail.com>
2024-04-16 16:03:02 -0700

    added projects class

---
+++ database/projectsClass.js
@@ -0,0 +1,105 @@
+import { MongoClient } from 'mongodb';
+//import needed database data
+import { config } from "./db.js";
+import { Users } from "./usersClass.js"
+
+/*
+SCHEMA FOR PROJECTS BELOW------------------------------------------------------
+
+project = {
+    _id: auto generated,
+    name: should be given as projectName (unique),
+    administrator: should be given as username,
+    members: initially empty list. members added by administrator
+    tasks: initially empty list. tasks added by administrator
+}
+-----------------------------------------------------------------------------*/
+
+
+class Projects {
+    //constructor sets uri database name and the collection to add to
+    constructor() {
+        this.uri = config.mongoURI;
+        this.dbName = config.dbName;
+        this.collectionName = config.projectsCollection;
+        this.users = new Users(this.uri);
+    }
+
+    // method used to create a new project based on username
+    async createProject(username, projectName) {
+        const client = new MongoClient(this.uri);
+        
+        try {
+            await client.connect();// Connect to MongoDB
+            //get the database
+            const database = client.db(this.dbName);
+            //get the collection
+            const collection = database.collection(this.collectionName);
+            const existingProject = await collection.findOne({name: projectName});
+            if(existingProject){
+                console.log(`A project with name: ${projectName} already exists please try again.`)
+                return false;
+            }else{
+                //initialize a new project
+                const newProject = {
+                    name: projectName,
+                    administrator: username,
+                    members: [],
+                    tasks: []
+                }
+                //insert the new user into the collection
+                const result = await collection.insertOne(newProject);
+                //insert the project name into the current users projects
+                this.users.addProjectToUser(username, projectName);
+
+                //print a success message
+                console.log(`project created for ${newProject.administrator} successfully.`);
+                return result.insertedId;
+            }
+            
+            } catch (error) {//if there was an error print the error
+            console.error('Error creating project:', error);
+            }finally {
+                await client.close(); // Close the connection
+        }
+    }
+
+    //method used for deleting a project
+    async deleteProject(username, projectName) {
+
+        const client = new MongoClient(this.uri);
+
+        try {
+            // Connect to MongoDB
+            await client.connect(); 
+            //get the database
+            const database = client.db(this.dbName);
+            //get the collection
+            const collection = database.collection(this.collectionName);
+
+            //delete project based on the username and name associated with project
+            const result = await collection.deleteOne({ administrator: username, name: projectName});
+            //delete the project from the users database
+            this.users.removeProjectFromUser(username, projectName);
+            //Check if the project was deleted successfully. deltedCount is a 
+            if (result.deletedCount === 1) {
+                console.log(`Project ${projectName} was deleted successfully by ${username}`);
+            } else {
+                console.log(`Failed to delete ${projectName}`);
+            }
+        } catch (error) {
+            //error checking
+            console.error('Error deleting project', error);
+        } finally {
+            //close connection
+            await client.close();
+        }
+    }
+}
+
+//usage example for adding a project
+const uri = config.mongoURI;
+const projects = new Projects(uri);
+
+await projects.createProject("asher", "test project");
+await projects.deleteProject("asher", "test project");
