50ac62a9ff8a090de703ec22a28a0c3502ef24eb
Asher Etherington <asherce21@gmail.com>
2024-04-16 13:18:28 -0700

    made index.js
    index.js is the main file for backend stuff it handles the application and any http requests that are made to the server that the app is running on. Also added some comments to some of the database files.

--- database/db.js
+++ database/db.js
@@ -1,5 +1,5 @@
 // uri for mongodb database
-module.exports = {
+export const config = {
     mongoURI: 'mongodb+srv://asherce21:A252478ssd@cluster0.1xxrv63.mongodb.net/',
     dbName: "slugmanager",
     usersCollection: "users"
---
+++ index.js
@@ -0,0 +1,29 @@
+// Main application file that handles backend tasks such as
+//   route handling and http request processing
+
+/* ---------- Assuming you have all needed packages installed -----------------------
+        use command: powershell -ExecutionPolicy Bypass -Command "nodemon index.js"
+                    in a vscode terminal to start up server
+-------------------------------------------------------------------------------------*/
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
+// dynamically get path to directory
+const __dirname = dirname(fileURLToPath(import.meta.url));
+// set main application variable using express
+const app = express();
+const port = 3000;//port to run on
+
+app.use(bodyParser.urlencoded({extended: true}));//bodyparser can now parse forms
+
+// main route
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/templates/index.html");
+});
+
+//make app listen on a port and print out the url of the running
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
