87d761d0febf6e0e30867d2dc1765aa67773f607
Asher <asherce21@gmail.com>
2024-04-23 16:14:02 -0700

    working
    Users are able to register and login and the server stores the user state like whether they are logged in and who the current user is. The site will redirect you to register page if user is not currently logged in.

--- index.js
+++ index.js
@@ -12,7 +12,13 @@ import { fileURLToPath } from "url";                        // convert a file UR
 import bodyParser from "body-parser";                       // parse body of HTTP requests
 import { Users } from "./database/usersClass.js";           // import the Users class
 import { config } from "./database/db.js";                  // configuration settings
+import session from 'express-session';
+import { createHash } from 'crypto';
 
+// function for hashing passwords
+function hash(string) {
+    return createHash('sha256').update(string).digest('hex');
+} 
 
 /*---------------- express server setup -------------------------------------- */
 // dynamically get path to directory
@@ -25,26 +31,53 @@ const port = 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 // serve static files from the 'static' directory
 app.use(express.static(__dirname + "/static"));
-
+//set up the session in order to save user data like whether they are logged in or the current users username etc.
+app.use(session({
+    secret: 'slugmanager',
+    resave: false,
+    saveUninitialized: true,
+}));
 
 
 /* ---------------- VIEWS ---------------- */
 // main route; index page
 app.get("/", (req, res) => {
-    res.render(__dirname + "/templates/index.ejs");
+    //if user is logged in send to homepage otherwise send to register
+    if (req.session.isLoggedIn==true) {
+        res.render(__dirname + "/templates/index.ejs");
+    }else{
+        res.redirect("/register");
+    } 
 });
 
 // register page
 app.get("/register", (req, res) => {
-    res.render(__dirname + "/templates/register.ejs");
+    //on first time registration error should be false
+    res.render(__dirname + "/templates/register.ejs", {error: false});
 });
 
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
     const uri = config.mongoURI;                            // retrieve mongoDB URI
     const users = new Users(uri);                           // new instance of Users class
     const {username, password} = req.body;                  // extract user/pass from request body
-    users.createUser(username, password);
-    res.status(201).json({username, password});             // send 'created' response back to client
+    //create a new user
+    
+    //res.status(201).json({username, password});             // send 'created' response back to client
+    //sign in using the user info
+    try {
+        //make sure user doesnt already exist
+        let success = await users.createUser(username, password);
+        if (success) {
+            //if it was a new user send them to login page
+            res.redirect("/login");
+        } else {
+            //if it wasnt a new user send an error and reload
+            res.render(__dirname + "/templates/register.ejs", { error: "Invalid username or password" });
+        }
+    } catch (error) {//catch any weird unexpected errors
+        console.error("Login error:", error);
+        res.render(__dirname + "/templates/register.ejs", { error: "An error occurred during login. Please try again." });
+    }
 });
 
 // login page
@@ -52,12 +85,27 @@ app.get("/login", (req, res) => {
     res.render(__dirname + "/templates/login.ejs");
 });
 
-app.post("/login", (req, res) => {                        
+app.post("/login", async (req, res) => {
     const uri = config.mongoURI;
     const users = new Users(uri);
-    const {username, password} = req.body;
-    users.login(username, password);   
-    res.status(200).json({username, password})               
+    const { username, password } = req.body;
+
+    try {
+        // try to login, must use await to ensure the function finishes returning before doing other stuff
+        let success = await users.login(username, hash(password));  // Assuming hash is a function you've defined elsewhere
+        //if it worked send users to homepage if not send to login
+        if (success) {
+            //set session variables
+            req.session.isLoggedIn = true;
+            req.session.username = username;
+            res.redirect("/");
+        } else {
+            res.render(__dirname + "/templates/login.ejs", { error: "Invalid username or password" });
+        }
+    } catch (error) {//catch any weird unexpected errors
+        console.error("Login error:", error);
+        res.render(__dirname + "/templates/login.ejs", { error: "An error occurred during login. Please try again." });
+    }
 });
 
 // make app listen on a port and print out the url of the running app
